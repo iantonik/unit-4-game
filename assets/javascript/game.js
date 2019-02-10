a096b99490f9323be9de9f5c023b3c8ee0c0d884
Iwona Antonik <iwona.antonik@gmail.com>
2019-02-10 12:17:12 -0500

    code cleanup in progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,9 @@
+// To Do:
+// Game reset.
+// readme file describing project
+// update portfolio 
+//soft delete dude from the dictionary. 
+
 //hp = health points
 //ap = attack power
 //cap = counter attack power
@@ -9,6 +15,7 @@ var characters = {
         ap: 8,
         cap: 13,
         src: "assets/images/ObiWanKenobi.jpeg",
+        isActive: true,
     },
     luke: {
         name: "Luke Skywalker",
@@ -16,7 +23,7 @@ var characters = {
         ap: 7,
         cap: 9,
         src: "assets/images/LukeSkywalker.jpeg",
-
+        isActive: true,
     },
     sidious: {
         name: "Darth Sidious",
@@ -24,6 +31,7 @@ var characters = {
         ap: 6,
         cap: 11,
         src: "assets/images/DarthSidious.jpeg",
+        isActive: true,
     },
     maul: {
         name: "Darth Maul",
@@ -31,113 +39,118 @@ var characters = {
         ap: 5,
         cap: 15,
         src: "assets/images/DarthMaul.jpeg",
+        isActive: true,
     }
 }
 
+var player;
+var defender;
+var enemy = [];
+var powerIncrement;
+var playerHP;
+var defenderHP;
 
-function loadCharacters(){
-    for(key in characters){
-        $("<button/>",{
+function loadCharacters() {
+    for (key in characters) {
+        $("<button/>", {
             'class': `${key}  character`,
             'value': key,
-            'html': ('<img src='+characters[key].src+'>' + '<p>'+characters[key].name +'<p id='+key+'>'+ characters[key].hp),
-            
+            'html': `<img src="${characters[key].src}"/><p>${characters[key].name}</p><p id=${key}>${characters[key].hp}</p>`,
+
         }).prependTo("#setup")
     }
 }
 
-loadCharacters();
-
-
-
-
-var player = "";
-var defender = "";
-var enemy = [];
-var powerIncrement;
-
-
-
-$(document).on('click', '.character', function(){
-
-    if (!player) {
-        player = $(this).attr("value");
-        $(this).detach().appendTo('#player')
-        powerIncrement = characters[player].ap;
-        $(this).attr("disabled", "disabled").css("background-color", "#4b923a"); //disable button, can't change player.
-    
-     
-    } else if (!defender) {
-        defender = $(this).val();
-        $(this).detach().appendTo('#defender')
-        $(this).attr("disabled", "disabled").css("background-color", "#b3201a"); //disable button, can't change defender after selection.
-        populateEnemy();
-        maintainEnemy();
-
-
-    }
-})
-
 function populateEnemy() {
     for (var key in characters) {
-        if (key !== player && key !== defender && enemy.indexOf(key) === -1)
+        if (key !== player && enemy.indexOf(key) === -1)
             enemy.push(key);
     };
 }
+
 function maintainEnemy() {
     enemy = enemy.filter(function (e) {
         return e != defender;
-    })
+    });
     enemy.forEach(element => {
-        $("."+element).detach().appendTo('#enemy').css("background-color", "#81887f")
+        $(`.${element}`).detach().appendTo('#enemy').css("background-color", "#81887f");
     });
 }
 
-
-$(".attack").click(function () {
-    //player attack
-    characters[defender].hp = characters[defender].hp - characters[player].ap;
-    characters[player].ap = characters[player].ap + powerIncrement;
-    console.log(characters[player].ap);
-
-    //defender response
-    characters[player].hp = characters[player].hp - characters[defender].cap;
-
-    //update stats
+function inProgressStats() {
     $("#" + defender).html(characters[defender].hp);
     $("#" + player).html(characters[player].hp);
-    $("#ap").html("You've attacked with "+characters[player].ap+" attack points.");
-    $("#cap").html(characters[defender].name+" responded with "+characters[defender].cap+" counter attack points.");
-
-    gameStats();
-
-})
+    $("#note1").html(`You've attacked with ${characters[player].ap} attack points. <br> ${characters[defender].name} responded with ${characters[defender].cap} counter attack points.`);
+}
 
 function gameStats() {
-    if (characters[player].hp <=0 && characters[defender].ph<=0 ){
-        $("#ap").toggle().html("It's a draw!")
-        $("#cap").hide(); 
+    if (characters[player].hp <= 0 && characters[defender].hp <= 0) {
+        $("#note1").show().html("It's a draw!")
+        $(".attack").hide();
+        $(".restart").show();
     }
-    else if (characters[player].hp <= 0){
-        $("#ap").show().html("You lost! Game Over!")
-        $("#cap").hide();
-        $(".attack").attr("disabled", "disabled");
+    else if (characters[player].hp <= 0) {
+        $("#note1").show().html("You lost! Game Over!")
+        $(".attack").hide();
+        $(".restart").show();
     }
     else if (characters[defender].hp <= 0) {
-        $("."+defender).remove();
-        $("#ap").toggle();
-        $("#cap").toggle();
-        delete characters[defender];
+        $("." + defender).remove();
+        $("#note1").toggle();
+        // delete characters[defender];
         defender = "";
 
         if (!enemy.length) {
-            $("#ap").toggle().html("You Win!")
-            $("#cap").hide();
+            $("#note1").toggle().html("You Win!")
+            $(".attack").hide();
+            $(".restart").show();
         } else {
-            $("#ap").toggle().html("You won this round")
-            $("#cap").toggle().html("Select next defender!");
+            $("#note1").toggle().html("You won this round! <br> Select next defender to continue playing.")
         }
     }
 }
 
+function gameReset() {
+    for (var key in characters) {
+        $("."+key).remove();
+    };
+    player = "";
+    defender = "";
+    enemy = [];
+    powerIncrement=0;
+    loadCharacters()
+    $(".attack").show();
+    $(".restart").hide();
+    $("#note1").hide();
 
+}
+
+$(document).ready(function () {
+    loadCharacters();
+    $(document).on('click', '.character', function () {
+        if (!player) {
+            player = $(this).attr("value");
+            $(this).detach().appendTo('#player')
+            powerIncrement = characters[player].ap;
+            $(this).attr("disabled", "disabled").css("background-color", "#4b923a"); //disable button, can't change player.
+            populateEnemy();
+            maintainEnemy();
+
+        } else if (!defender) {
+            defender = $(this).val();
+            $(this).detach().appendTo('#defender')
+            $(this).attr("disabled", "disabled").css("background-color", "#b3201a"); //disable button, can't change defender after selection.
+            maintainEnemy();
+        }
+    })
+    $(".attack").click(function () {
+        characters[defender].hp -= characters[player].ap;
+        characters[player].hp -= characters[defender].cap;
+        inProgressStats();
+        characters[player].ap += powerIncrement;
+        gameStats();
+    })
+    $(".restart").click(function(){
+        gameReset();
+    })
+});
